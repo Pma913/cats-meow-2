af1f6fb391bbdec9f333842117ee2cb6af2e27c6
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-06-06 13:29:58 -0400

    Add styling to header component and hero image

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,13 +4,13 @@ import About from '../About/About'
 
 const Home = () => {
   return (
-    <section>
-      <div>
-        <img src="https://images.unsplash.com/photo-1589883661923-6476cb0ae9f2?ixlib
+    <section className="home">
+      <div className="hero">
+        <img className="hero-img" src="https://images.unsplash.com/photo-1589883661923-6476cb0ae9f2?ixlib
         =rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto
         =format&fit=crop&w=1074&q=80" alt="Cat lying on its back"/>
-        <button>Get Cat Facts</button>
-        <button>Get Favorites</button>
+        <button className="fact-btn">Get Cat Facts</button>
+        <button className="fav-btn">Get Favorites</button>
       </div>
       <div>
         <About />
