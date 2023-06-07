7ac67528733f19a8145537196a3a339d8540433c
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-06-06 20:59:33 -0400

    Create Card component and initialize its render

---
+++ src/components/Card/Card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Card.css';
+
+const Card = () => {
+  return (
+    <div>
+      <p></p>
+      <img src=""/>
+    </div>
+  )
+}
---
+++ src/components/Favorites/Favorites.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Favorites.css';
+
+const Favorites = () => {
+  return (
+    <section>
+       
+    </section>
+  )
+}
