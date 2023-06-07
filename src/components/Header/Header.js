86f3f417cd89d4eef3e900a1d825fff8e36ab6e6
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-06-07 09:52:05 -0400

    Add link to app logo to redirect to home page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
     <section className="header">
-      <img className="logo" src="./cats_meow.png" alt="Cute cat" /> 
+      <Link to="/"><img className="logo" src="./cats_meow.png" alt="Cute cat" /></Link> 
       <h1 className="app-title">Cats Meow</h1>
       <div className="nav-links">
         <NavLink className="link" to="/">home</NavLink>
