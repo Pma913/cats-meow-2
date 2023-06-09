a5e79f66c6144135e351041138c0c13e1c109769
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2023-06-09 11:55:22 -0400

    Build out test suit for errors and sad paths and update favorites component to display message if there are no saved ideas

--- cypress/e2e/error_spec.cy.js
+++ cypress/e2e/error_spec.cy.js
@@ -1,5 +1,16 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('error page', () => {
+  it('should display an error page if failed to get fact', () => {
+    cy.intercept('https://catfact.ninja/fact', {
+      statusCode: 500
+    })
+    cy.visit('http://localhost:3000/')
+    cy.get('button[class=fact-btn]').click()
+    cy.get('h2').contains('Looks like there was a problem. Please try again later')
   })
-})
+
+  it('should display message if no favorited facts', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('button[class=fav-btn]').click()
+    cy.get('h2').contains('No saved ideas yet')
+  })
+})  
