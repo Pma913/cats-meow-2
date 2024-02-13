e7bba0cec1ec12ef8074b977af312cb5edfcc109
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2024-02-13 17:16:39 -0500

    Update old test case for cypress testing to account for default behaviour of data router

--- cypress/e2e/fact_spec.cy.js
+++ cypress/e2e/fact_spec.cy.js
@@ -18,17 +18,19 @@ describe('visiting the fact page', () => {
     cy.get('button[class=new-fact-btn]').should('be.visible')
   })
 
-  // it('should display error when failed to fetch fact and provide a path back home', () => {
-  //   cy.visit('http://localhost:3000/fact')
-  //   cy.intercept('https://catfact.ninja/fact', {
-  //     status: 400,
-  //     fixture: 'cat-facts'
-  //   })
-  //   cy.get('button[class=new-fact-btn]').click()
-  //   cy.get('h2').contains('Looks like something is wrong. Please try again later.')
-  //   cy.get('a').click()
-  //   cy.get('h1').contains('Cats Meow')
-  // })
+  it('should display error when failed to fetch fact and provide a path back home', () => {
+    cy.visit('http://localhost:3000/fact')
+    cy.intercept('https://catfact.ninja/fact', {
+      statusCode: 404,
+      body: '404 Not Found!',
+      headers: {
+        'x-not-found': 'true',
+      },
+    })
+    cy.get('h2').contains('Looks like something is wrong. Please try again later.')
+    cy.get('a').click()
+    cy.get('h1').contains('Cats Meow')
+  })
 
   it('should maintain fact display over page refresh', () => {
     cy.clearAllSessionStorage()
