28a0f05a9820783942f230e17d1c0972ce96c572
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2024-03-06 16:29:54 -0500

    Add a trash icon to favorite cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './Card.css';
 import PropTypes from 'prop-types'; 
+import trashIcon from '../../utilities/trash-icon.png';
 
 const Card = ({ name, removeFav, id, image }) => {
   return (
     <div className="fav-card">
       <img src={image} alt={`${name} cat`} className="card-thumb" />
       <div className="card-btn-container">
-        <button className="delete-btn" onClick={() => {
-          removeFav(id);
-        }}>Delete</button>
+        <img src={trashIcon} className="delete-btn" onClick={() => removeFav(id)} />
         <button className="expand-button">Expand</button>
       </div>
       <p className="fav-card-name">{name}</p>
