09b3348c1974a4492300f528ccdc7c8b9f3ae617
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2024-03-05 21:46:33 -0500

    Update data cleaner to assign unique ids to each cat

--- src/utilities/dataCleaner.js
+++ src/utilities/dataCleaner.js
@@ -1,11 +1,14 @@
 const dataCleaner = (catApiData) => {
+  let counter = 0
+  
   return catApiData.map(cat =>{
+    counter++
     return {
       affectionLevel: cat.breeds[0].affection_level,
       description: cat.breeds[0].description,
       dogFriendly: cat.breeds[0].dog_friendly >= 5 ? "Yes" : "No",
       energyLevel: cat.breeds[0].energy_level,
-      id: Date.now(),
+      id: (Date.now() + counter),
       image: cat.url,
       name: cat.breeds[0].name,
       origin: cat.breeds[0].origin,
