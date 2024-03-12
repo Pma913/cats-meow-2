3361583540e78ad03e8c58d280a9795022c66592
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2024-03-12 11:05:10 -0400

    Style card name to look like the appropriate title for each card

--- src/components/FactCard/FactCard.js
+++ src/components/FactCard/FactCard.js
@@ -11,7 +11,7 @@ const FactCard = ({ details, favFact, removeFav, leftArrow, rightArrow }) => {
     <div className="fact-display">
       <div className="cat-specs">
         <div className="cat-details">
-          <p className="name">{`Name: ${details.name}`}</p>
+          <p className="name">{details.name}</p>
           <p className="dog-friendly">{`Dog Friendly: ${details.dogFriendly}`}</p>
           <p className="energy-level">{`Energy Level: ${details.energyLevel}`}</p>
           <p className="affection-level">{`Affection Level: ${details.affectionLevel}`}</p>
