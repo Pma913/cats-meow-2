8099107fa2f2a909813bff07512e87d80a6b914b
Patrick Ankiewicz <patrickmankiewicz@gmail.com>
2024-03-13 22:28:28 -0400

    Add home buttons to a container and include background-color

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,8 +15,10 @@ const homeCat = screenWidth > 1000 ? bigKitty : kitty;
     <section className="home">
       <div className="hero">
         <img className="hero-img" src={homeCat} alt="Cat lying on its back"/>
-        <Link to="/fact"><button className="fact-btn">Get Cat Facts</button></Link>
-        <Link to="/favorites"><button className="fav-btn">Get Favorites</button></Link>
+        <div className="home-btn-con">
+          <Link to="/fact"><button className="fact-btn">Get Cat Facts</button></Link>
+          <Link to="/favorites"><button className="fav-btn">Get Favorites</button></Link>
+        </div>
       </div>
       <div>
         <About />
